148f3657dc7cd46f6ee7d2bd716cc1ccc5610631
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-10-14 21:50:40 +0900

    aboutSpain

---
+++ js/mainSlide.js
@@ -0,0 +1,17 @@
+const mainContainer = document.querySelector('.slide-area');
+const mainSlide = document.querySelectorAll('.main_slide');
+const rightBtn = document.querySelector('.left-btn');
+const leftBtn = document.querySelector('.right-btn');
+
+
+leftBtn.addEventListener('click', function(){
+    console.log('click');
+})
+
+rightBtn.addEventListener('click', function(){
+    console.log('click');
+})
+
+function rightMove(){
+
+}
