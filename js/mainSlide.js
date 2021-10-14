e411fb942aaa67e0113ce3648ee4f8c40d4388fc
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-10-15 00:04:29 +0900

    aboutSpain

--- js/mainSlide.js
+++ js/mainSlide.js
@@ -1,17 +1,57 @@
 const mainContainer = document.querySelector('.slide-area');
-const mainSlide = document.querySelectorAll('.main_slide');
+const mainSlides = document.querySelectorAll('.main_slide');
+
+// ㅂㅓㅌㅌㄴㄴ
 const rightBtn = document.querySelector('.left-btn');
 const leftBtn = document.querySelector('.right-btn');
 
+//counter
+ let counter =1;
+ const size = mainSlides[0].clientWidth;
+ console.log(size);
+
+mainContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+console.log(mainContainer.style.transform);
+ // buttonlisteners
+
+rightBtn.addEventListener('click', function() {
+    if(counter >= mainSlides.length -1) return;
+    mainContainer.style.transition = 'transform 0.7s ease-in-out';
+    counter++;
+    mainContainer.style.transform = 'translateX(' + ( -size * counter ) +  'px)';
+
+ });
 
 leftBtn.addEventListener('click', function(){
-    console.log('click');
-})
+    if(counter <= 0) return;
+    mainContainer.style.transition = 'transform 0.7s ease-in-out';
+    counter--;
+    mainContainer.style.transform = 'translateX(' + ( -size * counter ) +  'px)';
 
-rightBtn.addEventListener('click', function(){
-    console.log('click');
-})
+ });
 
-function rightMove(){
+ mainContainer.addEventListener('transitionend',function() {
+     console.log('움직임');
+     if(mainSlides[counter].id === 'lastClone'){
+        mainContainer.style.transition = 'none';
+        counter = mainSlides.length - 2;
+        mainContainer.style.transform = 'translateX(' + ( -size * counter ) +  'px)';
+     }
 
+     if(mainSlides[counter].id === 'firstClone'){
+        mainContainer.style.transition = 'none';
+        counter = mainSlides.length - counter;
+        mainContainer.style.transform = 'translateX(' + ( -size * counter ) +  'px)';
+     }
+ });
+
+var timer = undefined;
+function autoSilde(){
+    if(timer == undefined){
+        timer = setInterval(function(){
+            counter = mainSlides.length - 2;
+        }, 2000);
+    }
 }
+autoSilde();
+
