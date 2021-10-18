8732f60c3ef076779eeb186ce665cc099ebc78d8
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-10-18 18:54:08 +0900

    spain

--- js/festival.js
+++ js/festival.js
@@ -1,7 +1,11 @@
 let bgSection = document.querySelectorAll('.bg-section > .festival-area');
 let festivalListBtn = document.querySelectorAll('.festivalMenu > ul > li')
+let festivalImage = document.querySelectorAll('.bg-section > .festival-list > img')
+
 bgSection = Array.prototype.slice.call(bgSection);
 festivalListBtn = Array.prototype.slice.call(festivalListBtn);
+festivalImage = Array.prototype.slice.call(festivalImage);
+
 
 for(let i=0; i< bgSection.length; i++){
     festivalListBtn[i].addEventListener('click', function(e){
@@ -10,7 +14,9 @@ for(let i=0; i< bgSection.length; i++){
         console.log(index, target);
         for(let j = 0; j < festivalListBtn.length; j++){
             bgSection[j].classList.remove('active');
+           
         }
         bgSection[index].classList.add('active');
     })
 }
+
