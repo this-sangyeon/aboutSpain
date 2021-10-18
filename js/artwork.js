d84c4dd50fc7faa511cb9655e569146b0fc592fb
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-10-18 21:52:37 +0900

    spain

---
+++ js/artwork.js
@@ -0,0 +1,24 @@
+let artworkImage = document.querySelectorAll('.artwork > img');
+let artworkImageTitle = document.querySelectorAll('.artwork > div');
+
+artworkImage = Array.prototype.slice.call(artworkImage);
+artworkImageTitle = Array.prototype.slice.call(artworkImageTitle);
+
+
+
+
+for(let i = 0; i < artworkImage.length; i++){
+    artworkImage[i].addEventListener('mouseover',function(){
+        for(let i = 0; i < artworkImage.length; i++){
+            artworkImageTitle[i].classList.add('active');
+        }
+    })
+}
+
+for(let i = 0; i < artworkImage.length; i++){
+    artworkImage[i].addEventListener('mouseleave',function(){
+        for(let i = 0; i < artworkImage.length; i++){
+            artworkImageTitle[i].classList.remove('active');
+        }
+    })
+}
